455119b618104be5c3d5325ff7c4acb9e84793d5
mpolitti <marcopolitti.br@gmail.com>
2024-09-05 16:26:20 -0300

    Implementada a exclusão de veículo

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -33,27 +33,34 @@ namespace DesafioFundamentos.Models
 
         public void RemoverVeiculo()
         {
-            Console.WriteLine("Digite a placa do veículo para remover:");
-
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
+            
             string placa = "";
+            
+            Console.WriteLine("Digite a placa do veículo para remover:");
+            placa = Console.ReadLine();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
+                
+                //int horas = 0;
+                decimal valorTotal = 0;
+
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                if (!(int.TryParse(Console.ReadLine(), out int horas))) {
+                 
+                    Console.WriteLine("Horas inválidas!");
+                    
+                } else {
+                
+                    valorTotal = precoInicial + precoPorHora * horas;
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                    veiculos.Remove(placa);
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                    Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
 
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                }
+
             }
             else
             {
